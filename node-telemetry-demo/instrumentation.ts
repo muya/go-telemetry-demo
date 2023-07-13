029396d63008eb49ce810541a6a6f7be55fc9c7c
Fred Muya <fred@yoco.com>
2023-07-13 11:04:41 +0300

    Configure Node Project

---
+++ node-telemetry-demo/instrumentation.ts
@@ -0,0 +1,14 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import {ConsoleMetricExporter, PeriodicExportingMetricReader} from "@opentelemetry/sdk-metrics";
+import {getNodeAutoInstrumentations} from "@opentelemetry/auto-instrumentations-node";
+
+const sdk = new NodeSDK({
+    traceExporter: new ConsoleSpanExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new ConsoleMetricExporter()
+    }),
+    instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start();
